a0ef0b8b94e3fa1b0113f56f4062eafaacbee1a6
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-18 12:13:49 -0500

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', newTask)
 });
+
+
+function newTask(event) {
+  event.preventDefault()
+  let input = event.target['new-task-description'].value
+  appendTask(input)
+  event.target['new-task-description'].value = ''
+}
+
+function appendTask(input) {
+  //selectors, elements, variables
+  let ul = document.querySelector('#tasks')
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+
+  //assign values
+  li.innerText = `${input} `
+  btn.innerText = 'X'
+
+  //appending to DOM
+  li.append(btn)
+  ul.appendChild(li)
+
+  //event listener
+  btn.addEventListener('click', deleteTask)
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove()
+}
